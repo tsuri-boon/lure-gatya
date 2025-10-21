5da8b8cd4b4558c9dbfeaafaaa24e0529e9dfdcc
tsuri-boon <bawbizup@outlook.com>
2025-10-21 23:57:31 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -131,7 +131,7 @@ spinBtn.addEventListener("click", () => {
          <div><a href="${selected.url}" target="_blank">${selected.url}</a></div>
           <p>ルアー名: ${randomLure.name}</p>
           <p>種類： ${randomLure.type}</p>
-          <div class="lure-image" style="margin-top:10px; width:100%; max-width:400px; height:auto;">
+          <div class="lure-image" style="margin-top:10px; width:100%; max-width:400px; height:auto; text-align:center;">
           ${randomLure.image}  
           </div>
           <div style="margin-top: 10px;">${selected.link}</div>
